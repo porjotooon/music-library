5b2ec40dc1fde24bbcc1e07a0ebf3ca9ece68b11
Dell <nafishasnain0@gmail.com>
2020-11-10 02:08:33 -0500

    Implemented dark theme with Material UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React, { useState } from 'react';
+import Switch from '@material-ui/core/Switch';
+
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global'
+import styled from 'styled-components';
+
 import Song from './components/Song';
 import Player from './components/Player';
 import './styles/app.scss'
@@ -6,7 +13,21 @@ import './styles/app.scss'
 //imported the music data
 import data from './util';
 
+
 function App() {
+  const [theme, setTheme] = useState('dark');
+
+  const [state, setState] = useState(true)
+
+  const toggleTheme = () => {
+    if(theme === 'light'){
+      setTheme('dark')
+      setState(true)
+    } else {
+      setTheme('light')
+      setState(false)
+    }
+  }
 
   // this state used to store list of songs
   const [songs, setSongs] = useState(data())
@@ -14,10 +35,16 @@ function App() {
   // this state used to show current song item
   const [currSong, setCurrSong] = useState(songs[0])
   return (
-    <div className="App">
-      <Song currSong={currSong}/>
-      <Player/>
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles/>
+        <Switch checked={state} onChange={toggleTheme} className="switch"/>
+        <div className="App">
+          <Song currSong={currSong}/>
+          <Player/>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
